22a6d77ec77363c352d1cde20d13cfd77118ffe7
Amrit kumar Mahto <amrit.mahto@adypu.edu.in>
2026-01-29 17:33:34 +0530

    tailwind applied

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-italic font-bold text-red-600">
+        RepoLens
+      </h1>
+    </div>
+  );
+}
